fbbaaf7962218b073003c83ee18eaffdf1282f16
malingbing <15638836857@163.com>
2018-05-23 17:30:09 +0800

    权限选中逻辑修改

--- microservice-front-app/src/main/resources/static/js/js.js
+++ microservice-front-app/src/main/resources/static/js/js.js
@@ -12,10 +12,10 @@ var ctxAppWeixin = "http://weixin.xrtz.org:8050/weixin";
 //
 //console.log(window.location.protocol+ "//" +"127.0.0.1");
 //
-ctxApp = "http://127.0.0.1:8022";
-ctxAppSec = "http://127.0.0.1:8050/sec";
-ctxAppSec2 = "http://127.0.0.1:8002";
-ctxAppWeixin = "http://127.0.0.1:8050/weixin";
+ctxApp = "http://192.168.0.192:8022";
+ctxAppSec = "http://192.168.0.192:8050/sec";
+ctxAppSec2 = "http://192.168.0.192:8002";
+ctxAppWeixin = "http://192.168.0.192:8050/weixin";
 //
 //
 //if(hostname == window.location.protocol + "//" +"127.0.0.1" || hostname == window.location.protocol + "//" +"localhost"){
