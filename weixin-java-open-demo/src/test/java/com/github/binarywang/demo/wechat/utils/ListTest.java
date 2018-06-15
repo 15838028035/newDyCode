a5ca8c2318cd412d14ea0743bb0ce80fee1713aa
wangyanke168 <443666413@qq.com>
2018-06-15 12:01:48 +0800

    无内容

--- weixin-java-open-demo/src/test/java/com/github/binarywang/demo/wechat/utils/ListTest.java
+++ weixin-java-open-demo/src/test/java/com/github/binarywang/demo/wechat/utils/ListTest.java
@@ -3,13 +3,21 @@ package com.github.binarywang.demo.wechat.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.weixindev.micro.serv.common.bean.weixin.WeixinImgtextItem;
-
 public class ListTest {
-
-	
+	@Autowired
+	StringEncryptor stringEncryptor;
+	private static final String KEY = "EbfYkitulv73I2p0mXI50JMXoaxZTKJ7"; 
 	public static void main(String []args) {
-		test();
+		//test();
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();  
+		encryptor.setPassword(KEY);  
+		System.out.println(encryptor.decrypt("BX68r192mXkhSUD6fRulCFhlLGA3SL5Q")); 
 	}
 	
 	public static  void test(){
